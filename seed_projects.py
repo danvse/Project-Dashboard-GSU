a68477fd9f79f2267cf29828a50cf48d1ef7b50a
danvse <141261361+danvse@users.noreply.github.com>
2026-02-11 22:45:06 -0500

    Update seed_projects.py

--- seed_projects.py
+++ seed_projects.py
@@ -6,7 +6,7 @@ import sys
 import os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'backend'))
 
-from app import app, db, User
+from backend.app import app, db, User
 from werkzeug.security import generate_password_hash
 
 # Project data
